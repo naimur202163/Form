8fb441a430b7c18a8c64bf516545c45e49e416b5
naimur <naimur202163@gmail.com>
2022-08-13 09:25:46 +0600

    react-hook form added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,52 @@
-import React from 'react'
-import Navigation from '../Navbar/Navbar'
-
+import React from "react";
+import Navigation from "../Navbar/Navbar";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import apple from "../../assets/google.jpg";
+import google from "../../assets/apple.jpg";
+import mask from '../../assets/Mask Group.png'
+import "./Home.css";
 export default function Home() {
   return (
     <div>
-        <Navigation/>
+      <Navigation />
+
+      <div className="content">
+        <Container>
+          <Row>
+            <Col >
+              <div>
+                <h2 className="banner-heading">
+                  Seamless Payments <br /> Experiences for <br />
+                  Everyone, Everywhere.
+                </h2>
+
+                <p>
+                  Get good energy off your money and your everyday transactions
+                  simplified with a MyChakra account. The MyChakra app allows
+                  you send and receive money globally, pay bills instantly and
+                  share gift tokens with friends and family.
+                </p>
+              </div>
+              <div className="app-logo">
+                <div >
+                  <img  className="logos" src={google} alt="" />
+                </div>
+                <div >
+                  <img  className="logos" src={apple} alt="" />
+                </div>
+              </div>
+            </Col>
+            <Col sm={6} xs={6}>
+                <div className="">
+                        <img src={mask} alt=""/>
+                </div>
+
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,53 +1,83 @@
 import React from "react";
 import "./SignIn.css";
+import { useForm } from "react-hook-form";
 export default function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log()
+    console.log(data)};
+
   return (
     <div className="form-control">
       <div>
         {/* Start Toggle Button */}
         <div className="py-5">
-            <input type="checkbox" id="switch" />
-            <div className="btn-toggle">
-              <label className="toggle-label" for="switch">
-                <div className="toggle"></div>
-                <div className="names">
-                  <p className="fan">Fan Signup</p>
-                  <p className="talent">Talent Signup</p>
-                </div>
-              </label>
-            </div>
-        
+          <input type="checkbox" id="switch" />
+          <div className="btn-toggle">
+            <label className="toggle-label" for="switch">
+              <div className="toggle"></div>
+              <div className="names">
+                <p className="fan">Fan Signup</p>
+                <p className="talent">Talent Signup</p>
+              </div>
+            </label>
+          </div>
         </div>
         {/* End Toggle Button */}
         <h3>Create Your Fun Accounte</h3>
         {/* from */}
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <label>Frist Name</label>
-          <input type="text" placeholder="firstName" />
+          <input
+            type="text"
+            placeholder="firstName"
+            {...register("frist_name")}
+          />
           <label>Last Name</label>
-          <input type="text" placeholder="lastname" />
+          <input
+            type="text"
+            placeholder="lastname"
+            {...register("last_name", { required: true })}
+          />
           <label>UserName</label>
-          <input type="text" placeholder="username" />
+          <input
+            type="text"
+            placeholder="username"
+            {...register("username", { required: true })}
+          />
           <label>Eamil</label>
-          <input type="email" placeholder="email" />
+          <input
+            type="email"
+            placeholder="email"
+            {...register("email", { required: true })}
+          />
           <label>password</label>
-          <input type="password" placeholder="password" />
-        </form>
-        <div className="submit">
-          <div>
-            <div className="checkBox">
-              <input type="checkBox" />
+          <input
+            type="password"
+            placeholder="password"
+            {...register("password", { required: true })}
+          />
+          <div className="submit">
+            <div>
+              <div className="checkBox">
+                <input type="checkBox" />
 
-              <bold>
-                <span className="agree">I agree to the</span>
-                <span className="conditions">Terms and conditions</span>
-              </bold>
-              <div className="btn">
-                <button type="submit">SignUp</button>
+                <bold>
+                  <span className="agree">I agree to the</span>
+                  <span className="conditions">Terms and conditions</span>
+                </bold>
+                <div className="btn">
+                  <button type="submit">SignUp</button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
